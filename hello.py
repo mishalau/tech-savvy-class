9dbcd2ecf12cd3cfe36e5cb6302578865beb9816
mishalau <mlau3@babson.edu>
2019-03-15 18:25:31 -0400

    finished class on Friday

---
+++ hello.py
@@ -0,0 +1,45 @@
+# print("hello,world!")
+#
+#  print('hello, Misha')
+#
+#  print(2019-2000)
+#
+# print(-2 + 100)
+
+# print('Hey Jude,' 'Do not make it bad')
+#
+# print('1 + 2 + 3 =', 1+2+3)
+#
+# name = input()
+#
+# print('Hello, ', name)
+
+# n=100
+# print(n)
+
+first_name = 'John'
+last_name = 'Lennon'
+name = first_name + " " + last_name
+# print ('hello,', name)
+# print ('hello, {}! You are {} years old!'.format(name,20))
+# template = 'hello, {}! You are {} years old!'
+
+name1 = 'Grace'
+age1 = 20
+#
+# name2 = 'Aida'
+# age2 = 18
+#
+# print(template.format(name1,age1))
+# print(template.format(name2,age2))
+
+# print('Pi equals {:.2f}'.format(3.14151926))
+
+# template = 'hello, {name}! You are {age} years old!'
+# print(template.format(age=age1, name=name1))
+
+# import math
+# print(math.pow(math.pi,5))
+# print(math.pi)
+
+print(2**38)
